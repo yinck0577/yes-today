34fb0178b1f46e823aa41a6492c38ad944f92fa7
yinck0577 <40005020+yinck0577@users.noreply.github.com>
2026-09-26 21:39:14 +0800

    Support optional article SEO keywords

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		keywords: z.array(z.string()).optional(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
